81c523d95bb622865d8c47c914f3fa2c83f37323
FelipeAssunsas <meframos1@gmail.com>
2025-11-27 13:43:54 -0300

    etapa 04 pt.4

--- src/terreno/terreno.h
+++ src/terreno/terreno.h
@@ -5,51 +5,50 @@
 
 class Terreno{
     float rugosidade;
-    int limite;
     Matriz<int> entradas;
-    void formatar(int inicio, int fim, float novo_l){
-        if(((fim - inicio) > 1) or ((inicio - fim) > 1)){
-            novo_l *= rugosidade;
-            diamond(inicio, fim);
-            square(inicio, fim);
-            formatar(inicio, (inicio + fim)/2, novo_l);
-            formatar((inicio+fim)/2, fim, novo_l);
-            //formatar((inicio+fim)/2, inicio, novo_l);
-            //formatar(fim, (inicio+fim)/2, novo_l);
+    void formatar(int inicio_l, int fim_l, int inicio_c, int fim_c, int novo_lim){
+        if((fim_l - inicio_l) > 1){
+            novo_lim *= rugosidade;
+            diamond(inicio_l, fim_l, inicio_c, fim_c, novo_lim);
+            square(inicio_l, fim_l, inicio_c, fim_c, novo_lim);
+            formatar(inicio_l, (inicio_l + fim_l)/2, inicio_c, (inicio_c + fim_c)/2, novo_lim); //Q2
+            formatar((inicio_l + fim_l)/2, fim_l, (inicio_c + fim_c)/2, fim_c, novo_lim); //Q4
+            formatar(inicio_l, (inicio_l + fim_l)/2, (inicio_c + fim_c)/2, fim_c, novo_lim); //Q1
+            formatar((inicio_l + fim_l)/2, fim_l, inicio_c, (inicio_c + fim_c)/2, novo_lim); //Q3
         }
     }
-    void diamond(int inicio, int fim){
-        int valor = (rand()%((2*limite) + 1) - limite);
-        int media = ((entradas(inicio,inicio)+entradas(inicio,fim)+entradas(fim,inicio)+entradas(fim,fim))/4);
-        entradas((fim+inicio)/2,(fim+inicio)/2) = media + valor;
+    void diamond(int inicio_l, int fim_l, int inicio_c, int fim_c, int novo_lim){
+        int valor = (rand()%((2*novo_lim) + 1) - novo_lim);
+        int media = ((entradas(inicio_c,inicio_l)+entradas(inicio_c,fim_l)+entradas(fim_c,inicio_l)+entradas(fim_c,fim_l))/4);
+        entradas((fim_c+inicio_c)/2,(fim_l+inicio_l)/2) = media + valor;
     }
-    void square(int inicio, int fim){
-        int valor, media, centro = entradas((fim+inicio)/2, (fim+inicio)/2);
-        valor = (rand()%((2*limite) + 1) - limite);
-        media = ((entradas(inicio, inicio) + entradas(inicio,fim) + centro)/3);
-        entradas(inicio,(fim+inicio)/2) = media + valor;
-        valor = (rand()%((2*limite) + 1) - limite);
-        media = ((entradas(inicio, inicio) + entradas(fim, inicio) + centro)/3);
-        entradas((fim+inicio)/2, inicio) = media + valor;
-        valor = (rand()%((2*limite) + 1) - limite);
-        media = ((entradas(fim, inicio) + entradas(fim,fim) + centro)/3);
-        entradas(fim, (fim+inicio)/2) = media + valor;
-        valor = (rand()%((2*limite) + 1) - limite);
-        media = ((entradas(inicio, fim) + entradas(fim,fim) + centro)/3);
-        entradas((fim+inicio)/2, fim) = media + valor;
+    void square(int inicio_l, int fim_l, int inicio_c, int fim_c, int novo_lim){
+        int valor, media, centro = entradas((fim_c+inicio_c)/2, (fim_l+inicio_l)/2);
+        valor = (rand()%((2*novo_lim) + 1) - novo_lim);
+        media = ((entradas(inicio_c, inicio_l) + entradas(inicio_c,fim_l) + centro)/3);
+        entradas(inicio_c,(fim_l+inicio_l)/2) = media + valor;
+        valor = (rand()%((2*novo_lim) + 1) - novo_lim);
+        media = ((entradas(inicio_c, inicio_l) + entradas(fim_c, inicio_l) + centro)/3);
+        entradas((fim_c+inicio_c)/2, inicio_l) = media + valor;
+        valor = (rand()%((2*novo_lim) + 1) - novo_lim);
+        media = ((entradas(fim_c, inicio_l) + entradas(fim_c,fim_l) + centro)/3);
+        entradas(fim_c, (fim_l+inicio_l)/2) = media + valor;
+        valor = (rand()%((2*novo_lim) + 1) - novo_lim);
+        media = ((entradas(inicio_c, fim_l) + entradas(fim_c,fim_l) + centro)/3);
+        entradas((fim_c+inicio_c)/2, fim_l) = media + valor;
     }
     public:
         Terreno(int n = 0, float rug = 0.5){
             rugosidade = rug;
             int x = pow(2, n) + 1;
             entradas = Matriz<int> (x, x);
-            limite = x;
+            int limite = x / 2;
             srand(time(0));
             entradas(0,0) = (rand()%((2*limite) + 1) - limite);
             entradas((x-1),0) = (rand()%((2*limite) + 1) - limite);
             entradas(0,(x-1)) = (rand()%((2*limite) + 1) - limite);
             entradas((x-1),(x-1)) = (rand()%((2*limite) + 1) - limite);
-            formatar(0, x-1, limite);
+            formatar(0, x-1, 0, x-1, limite);
         }
         int obterLargura(){
             return entradas.getColunas();
